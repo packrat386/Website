7503033919f36f3c1312583c3da3c2cb6b31ce6c
packrat386 <packrat386@gmail.com>
2014-01-08 20:46:44 -0500

    fixed the under construction page to look decent

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
+
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var path = require("path");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
+app.configure(function() {
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.logger("short"));
+});
+
 app.get('/', function(req, res) {
-  res.sendfile('index.html');
+    res.sendfile("index.html");
 });
 
 var port = process.env.PORT || 5000;
