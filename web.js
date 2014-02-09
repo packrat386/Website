6b125dce9ee849e76131215c8be19b15a71df515
Packrat386 <packrat386@gmail.com>
2014-02-09 05:05:36 -0500

    Re-did the site with jade instead of html

--- web.js
+++ web.js
@@ -14,35 +14,58 @@ app.use(logfmt.requestLogger());
 
 app.configure(function() {
     app.use(express.static(path.join(__dirname, 'public')));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
     app.use(express.json());
     app.use(express.urlencoded());
     app.use(express.logger("short"));
+    app.use(require('stylus').middleware(__dirname + 'public'));
 });
 
 app.get('/', function(req, res) {
-    res.sendfile("index.html");
+    res.render('index', {
+	title: 'index'
+    });
 });
 
 app.get('/BracketDistTool', function(req,res) {
-    res.sendfile("distTool.html");
+    res.render('bracketDistTool', {
+	title: 'bracketDistTool',
+    });
 });
 
 app.post('/BracketDistTool', function(req,res) {
-    parser.getPageData(req.body.page, function(page){
-	var code = '';
-	console.log("we're in a callback!");
-	if(page.query.pages != undefined){
-	    for(i in page.query.pages){
-		if(page.query.pages[i].revisions != undefined){
-		    code = parser.parsePage(page.query.pages[i].revisions[0]['*']);
-		    res.send(fragments.codeHeader + "<p>Table for: " + req.body.page + "</p><pre>" +  code + "</pre>" + fragments.codeFooter);
-		}else{
-		    res.sendfile("distToolError.html");
+    if(/wiki.teamliquid.net\/starcraft2\/(.*)/.test(req.body.page)){
+	var result = /wiki.teamliquid.net\/starcraft2\/(.*)/.exec(req.body.page);
+	var pageTitle = result[1];
+	console.log(pageTitle);
+	parser.getPageData(pageTitle, function(page){
+	    var code = '';
+	    console.log("we're in a callback!");
+	    if(page.query.pages != undefined){
+		for(i in page.query.pages){
+		    if(page.query.pages[i].revisions != undefined){
+			code = parser.parsePage(page.query.pages[i].revisions[0]['*']);
+			res.render('distToolOut', {
+			    title: pageTitle,
+			    output: code 
+			});
+		    }else{
+			res.render('bracketDistTool', {
+			    title: 'bracketDistTool',
+			    errorMsg: 'There was an error in your input, please try again' 
+			});
+		    }
 		}
-	    }
-	}	    
-    }); 
+	    }	    
+	}); 
+    }
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
